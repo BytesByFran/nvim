d69b2f1c9158958ed90e1723385fec4e1ab71627
FranJF <franjcfernandez@gmail.com>
2026-04-09 15:49:54 +0200

    improved: keymaps and lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,40 +1,37 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    opts = { ensure_installed = { "pylsp", "ruff", "stylua" } },
-    config = true,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    opts = { auto_install = true },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      --local lspconfig = require("lspconfig")
-      vim.lsp.config('pylsp', {})
-      vim.lsp.config('ruff', {
-            init_options = {
-                settings = {
-                    organizeImports = false,
-                },
-            },
+	{
+		"williamboman/mason.nvim",
+		opts = { ensure_installed = { "pylsp", "ruff", "stylua" } },
+		config = true,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = { auto_install = true },
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			--local lspconfig = require("lspconfig")
+			vim.lsp.config("pylsp", {})
+			vim.lsp.config("ruff", {
+				init_options = {
+					settings = {
+						organizeImports = false,
+					},
+				},
+			})
 
+			vim.lsp.enable("pylsp")
+			vim.lsp.enable("ruff")
 
-            })
-
-      vim.lsp.enable('pylsp')
-      vim.lsp.enable('ruff')
-
-      -- Teclas de acceso rápido (LSP)
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = "LSP: Información" })
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = "LSP: Ir a definición" })
-      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = "LSP: Acciones" })
-      vim.keymap.set('n', '<leader>tr', vim.lsp.buf.references, { desc = "LSP: Referencias" })
-      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = "LSP: Renombrar" })
-      vim.keymap.set('n', '<leader>n', vim.diagnostic.goto_next, { desc = "LSP: Siguiente diagnóstico" })
-      
-
-    end,
-  },
+			-- Teclas de acceso rápido (LSP)
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP: Información" })
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "LSP: Ir a definición" })
+			vim.keymap.set("n", "<leader>o", vim.lsp.buf.hover, { desc = "LSP: Información" })
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP: Acciones" })
+			vim.keymap.set("n", "<leader>tr", vim.lsp.buf.references, { desc = "LSP: Referencias" })
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP: Renombrar" })
+			vim.keymap.set("n", "<leader>n", vim.diagnostic.goto_next, { desc = "LSP: Siguiente diagnóstico" })
+		end,
+	},
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,36 +1,37 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter-context",
-    opts = { max_lines = 3 },
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    version = false, -- last release is way too old and doesn't work on Windows
-    build = ":TSUpdate",
-    main = "nvim-treesitter.configs", -- Very important for lazy.nvim to call setup correctly
-    opts = {
-      ensure_installed = {
-        "bash",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "tsx",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "yaml",
-      },
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = { enable = true , disable = { "python" }},
-    },
-  },
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		opts = { max_lines = 3 },
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		version = false, -- last release is way too old and doesn't work on Windows
+		build = ":TSUpdate",
+		main = "nvim-treesitter.configs", -- Very important for lazy.nvim to call setup correctly
+		opts = {
+			ensure_installed = {
+				"bash",
+				"html",
+				"javascript",
+				"json",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"query",
+				"regex",
+				"tsx",
+				"typescript",
+				"vim",
+				"vimdoc",
+				"yaml",
+				"go",
+			},
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+			indent = { enable = true, disable = { "python" } },
+		},
+	},
 }
